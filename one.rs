afce10001e2d642cd458c792ad7befa3c95344cb
xiaoyong <54741819+xiaoyong-wang@users.noreply.github.com>
2024-11-01 14:52:57 -0400

    update

---
+++ one.rs
@@ -0,0 +1,45 @@
+// use rustc one.rs to compile this file
+// then ./one to run the compiled file
+
+fn main() {
+    foo();
+    boo();
+    hooo();
+    goo();
+    shoo();
+    mooooo();
+    jooooo();
+    koooo();
+}
+
+fn foo() {
+    println!("Hello world!");
+}
+
+fn boo() {
+    println!("booo!!!")
+}
+
+fn hooo() {
+    println!("hooooooo!!!")
+}
+
+fn goo() {
+    println!("gooooooo!!!")
+}
+
+fn shoo() {
+    println!("shoooooo!!")
+}
+
+fn mooooo() {
+    println!("moooooo!!")
+}
+
+fn jooooo() {
+    println!("joooooo!!")
+}
+
+fn koooo() {
+    println!("koooooo!!")
+}
