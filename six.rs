77a316bbeac3c8ac280756d129723ee89fa8b7e8
xiaoyong <54741819+xiaoyong-wang@users.noreply.github.com>
2024-11-03 17:18:07 -0500

    updates

---
+++ six.rs
@@ -0,0 +1,8 @@
+fn main () {
+    println!("Control flow in Rust is similar to other programming languages.");
+    println!("Rust has the following control flow statements:");
+    println!("-------------------------------------------------------");
+    println!("if");
+    println!("if else");
+    println!("loop");
+}
